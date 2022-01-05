21b4d1b16110ddb42cde81b50188a85c2a207792
luiggy macias <ferrinluiggy@gmail.com>
2022-01-05 09:44:28 -0500

    adding login checks

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,9 +1,11 @@
 import pool from "../db";
+import { hash } from "bcryptjs";
 export const getUsers = async (req, res) => {
   try {
-    const { rows } = await pool.query("SELECT * FROM users");
+    // const { rows } = await pool.query("SELECT * FROM users");
+    const { rows } = await pool.query("SELECT user_id, email FROM users");
     console.log(rows);
-    res.json(rows);
+    return res.status(201).json({ success: true, users: rows });
   } catch (error) {
     let e = error as ErrorEvent;
     console.log(e.message);
@@ -11,14 +13,19 @@ export const getUsers = async (req, res) => {
 };
 
 export const register = async (req, res) => {
+  const { email, password } = req.body;
   try {
-    // const { rows } = await pool.query("SELECT * FROM users");
-    // console.log(rows);
-    console.log("validation pass");
-
-    // res.json(rows);
-  } catch (error) {
+    const hashedPassword = await hash(password, 10);
+    const { rows } = await pool.query(
+      "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+      [email, hashedPassword]
+    );
+    return res
+      .status(201)
+      .json({ success: "User created successfully", data: rows[0] });
+  } catch (error: unknown) {
     let e = error as ErrorEvent;
     console.log(e.message);
+    return res.status(500).json({ error: e.message });
   }
 };
--- backend/src/validators/auth.validator.ts
+++ backend/src/validators/auth.validator.ts
@@ -1,5 +1,7 @@
 import { check } from "express-validator";
 import db from "../db";
+import { compare } from "bcryptjs";
+// It will validate the email and password, this check are made in the middleware auth.middleware.ts file, after this validation are called in the auth.route.ts file, when are handled, then the validationMiddleware function is called, that is when the checks are made so the next middleware is called if the validation is ok.
 // varification of the user's password
 const password = check("password")
   .isLength({ min: 6, max: 15 })
@@ -10,6 +12,7 @@ const email = check("email")
   .isEmail()
   .withMessage("Please enter a valid email");
 
+// check if the user's email is already in the database
 const emailExists = check("email").custom(async (value) => {
   const { rows } = await db.query("SELECT * FROM users WHERE email = $1", [
     value,
@@ -19,6 +22,21 @@ const emailExists = check("email").custom(async (value) => {
   }
 });
 
+// login validation
+const loginFieldsCheck = check("email").custom(async (value, { req }) => {
+  // return console.log(req.body);
+  const { rows } = await db.query("SELECT * FROM users WHERE email = $1", [
+    value,
+  ]);
+  if (!rows[0]) {
+    throw new Error("Email does not exist");
+  }
+  const validPassword = await compare(req.body.password, rows[0].password);
+  if (!validPassword) {
+    throw new Error("Password is incorrect");
+  }
+});
 export default {
   registerValidation: [email, password, emailExists],
+  loginValidation: [loginFieldsCheck],
 };
